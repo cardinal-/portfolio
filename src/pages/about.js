1640faf4b19d847fa910ace239bf4a130d8cf26f
cardinal <mattcardinal@us.ibm.com@cardinals-mbp.austin.ibm.com>
2020-03-06 12:01:19 -0600

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I was born a poor gypsy child </p>
+    </div>
+  )
+}
+export default About
