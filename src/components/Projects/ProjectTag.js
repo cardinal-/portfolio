3d11f5fe6f3cda59a5c857e9dbd36c3cd88c8db1
Matt Cardinal <matt@mattcardinal.com>
2020-08-28 10:32:13 -0500

    added project tag

--- src/components/Projects/ProjectTag.js
+++ src/components/Projects/ProjectTag.js
@@ -3,6 +3,12 @@ import { colors } from '../../styles'
 
 const ProjectTag = styled.div`
   color: ${colors.secondaryColor};
+  font-size: 1.2rem;
+  padding: 3rem 0;
+
+  time {
+    margin-left: 5rem;
+  }
 `
 
 export default ProjectTag
